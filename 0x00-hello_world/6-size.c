2a99e108071ca145314816d29f1542adfb496832
Isaac-7 <isaacokai20@gmail.com>
2022-09-08 11:27:00 -0700

    h

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,7 +17,7 @@ int main(void)
 
 	 printf("Size of a char: %id bytes(s)\n", sizeof(c));
 	 printf("Size of an int: %id byte(s)\n", sizeof(i));
-	 printof("Size of a long int: %id byte(s)\n", sizeof(li));
+	 printf("Size of a long int: %id byte(s)\n", sizeof(li));
          printf("Size of a long long int: %id byte(s)\n", sizeof(lli));
          printf("Size of a float: %id byte(s)\n", sizeof(f));
 	 return (0);
