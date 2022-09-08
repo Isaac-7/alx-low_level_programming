e6b80e1bd03c05bceb649cb53172073b9aff1155
Isaac-7 <isaacokai20@gmail.com>
2022-09-08 11:16:21 -0700

    h

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the print function
+*
+*Description: using the main function
+*this program prints the various types
+*Return: 0
+*/
+int main(void)
+{
+         char cp;
+         int i;
+	 long li;
+	 long long lli;
+	 float f;
+
+	 printf("Size of a char: %id bytes(s)\n", sizeof((c));
+		printf("Size of an int: %id byte(s)\n", sizeof((i));
+		       printof("Size of a long int: %id byte(s)\n", sizeof((li));
+         printf("Size of a long long int: %id byte(s)\n", sizeof((li));
+         printf("Size of a float: %id byte(s)\n", sizeof((f));
+		return (0);
+}
