b97817f09bbf57d251d3ce76f6fd33924db44a60
Isaac-7 <isaacokai20@gmail.com>
2022-09-08 10:41:00 -0700

    h

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ *this program prints "with proper grammar, but the outcome is a piece of art"
+ */
+
+int main (void)
+{
+  printf("with proper grammar, but the outcome is a piece of art");
+    return (0);
