a1d7f645960c85ce3a6cd453a523f444ba882ef7
Isaac-7 <isaacokai20@gmail.com>
2022-09-09 10:26:31 -0700

    h

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -print all single digit numbers
+ *
+ * Description: using the main function
+ * this program prints ' 023456789'
+ * Return: 0
+ */
+int main (void)
+{
+  
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print alphabets in reverse
+ * 
+ * Description: using the main function
+ * this program prints zyxwvutsrqponmlkjihgfedcba
+ * Return: 0
+ */
+int main(void)
+{
+  char c;
+  for (c = 'z' c >= 'a'; c--)
+    {
+      putchar(c);
+    }
+  putchar("\n");
+  return (0);
+}
