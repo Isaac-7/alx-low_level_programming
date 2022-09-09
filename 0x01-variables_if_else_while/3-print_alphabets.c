c8a5c90296211a988d07416855bc18eba9eaa26a
Isaac-7 <isaacokai20@gmail.com>
2022-09-09 09:38:18 -0700

    h

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,13 @@
 int main (void)
 {
   char c;
-  for (char c = 'a'; c <= 'z'; c++)
+  for ( c = 'a'; c <= 'z'; c++)
     {
       putchar(c);
 	}
 
     
-      for (char c = 'A'; c <= 'Z'; c++)
+  for ( c = 'A'; c <= 'Z'; c++)
 	{
 	  putchar(c);
 	}
