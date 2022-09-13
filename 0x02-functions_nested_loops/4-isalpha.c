4b7136a83e3c92ac601b1a652026261769651ccc
Isaac-7 <isaacokai20@gmail.com>
2022-09-13 11:34:41 -0700

    h

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isaplha - checks for alphabets
+ * @c: argument
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (C >= 'A' && C <= 'Z'))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
+ 
