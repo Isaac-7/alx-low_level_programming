b3936c68c4acd3698170cc38a683369d6c6c9d93
Isaac-7 <isaacokai20@gmail.com>
2022-09-13 11:58:29 -0700

    h

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,25 @@
 #include "main.h"
 /**
- * print sign - check number sign
+ * print_sign - check number sign
  * @n: argument for function
  * Return: 0
  */
 int print_sign(int n)
 {
 	if (n > 0)
-	{ 
+	{	
 		_putchar('+');
 		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		return(-1);
+		return (-1);
 	}
 	else
 	{
 		_putchar('0');
-		return(0);
+		return (0);
 	}
 }
 
