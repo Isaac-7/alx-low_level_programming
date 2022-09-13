60a7ef5e1baf540befc159e282dcc06350f56b90
Isaac-7 <isaacokai20@gmail.com>
2022-09-13 15:00:58 -0700

    H

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints from input to 98 with comma and space
+ * @n: counting number
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n--);
+		}
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+		{
+			printf("%d, " n++);
+		}
+		printd("%d\n", n);
+	}
+}
+
+
+		
