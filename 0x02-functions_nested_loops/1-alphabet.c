204f882820bd5fb409e71a01d86d137b14830516
Isaac-7 <isaacokai20@gmail.com>
2022-09-13 10:21:49 -0700

    H

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,10 @@
  * 
  * Return: Always 0
  */
-
-void print_alphabet (void)
+void print_alphabet(void)
 {	char c;
 
-	for  (c ='a'; c <='z'; c++)
+	for  (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
 	}
