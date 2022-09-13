2939bcb24e924cf14cbb20fe2c482c744c47148b
Isaac-7 <isaacokai20@gmail.com>
2022-09-13 14:43:09 -0700

    h

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * add -adds two integrs
- * @a - first int
- * @b - second in
- * 
+ * @a: first int
+ * @b: second in
+ *
  * Return: addition result
  */
 int add(int a, int b)
