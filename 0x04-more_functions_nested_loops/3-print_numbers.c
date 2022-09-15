d1c02664036b4fbe8aeb83d42107ce920f8ed48e
Isaac-7 <isaacokai20@gmail.com>
2022-09-15 16:39:30 -0700

    h

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ *
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	int n;
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+}
