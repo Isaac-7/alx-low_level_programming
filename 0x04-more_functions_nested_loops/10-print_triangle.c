f46190863fc47aab0591654279c33b2c77ca7c78
Isaac-7 <isaacokai20@gmail.com>
2022-09-15 22:04:49 -0700

    h

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,7 +7,7 @@
  * Return: nothing
  */
 
-void print_triangle(int size);
+void print_triangle(int size)
 {
 	int a;
 	int b;
@@ -15,22 +15,22 @@ void print_triangle(int size);
 	if (size > 0)
 	{
 		for ( a = 1; a <= size; a++)
-	{
-		for ((b = size - a); b > 0; b--)
 		{
-			putchar(' ');
+			for ((b = size - a); b > 0; b--)
+			{
+				putchar(' ');
+			}
+			for (b = 0; b < a; b++)
+			{
+				putchar('#');
+			}
+			if (a == size)
+			{
+				continue;
+			}
+			putchar('\n');
 		}
-		for (b = 0; b < a; b++)
-		{
-			putchar('#');
-		}
-		if (a == size)
-		{
-			continue;
-		}
-		putchar('\n');
 	}
-	}
-putchar('\n');
-}
+	putchar('\n');
+
 
