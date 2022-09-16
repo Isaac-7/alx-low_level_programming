986cb680e2e0df40205f113ce0a97f835658bf42
Isaac-7 <isaacokai20@gmail.com>
2022-09-15 17:31:54 -0700

    h

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,7 +16,11 @@ void more_numbers(void)
 	{
 		for (b = 0; b <= 14; b++)
 		{
-			putchar(b);
+			if (b > 9)
+			{
+				putchar((b / 10) + '0');
+			}
+			putchar((b % 10) + '0');
 		}
 	putchar('\n');
 	}
