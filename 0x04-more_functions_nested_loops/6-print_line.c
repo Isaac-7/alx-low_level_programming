26bcf49292b3c8392457770f7880b90a2432e488
Isaac-7 <isaacokai20@gmail.com>
2022-09-15 17:40:47 -0700

    m

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - prints line
+ * @n: number of - to print
+ * Return: nothing
+ */
+
+void print_line(int n);
+{
+	for (i = 0; i <= n; i++)
+	{
+		putchar('_');
+	}
+}
