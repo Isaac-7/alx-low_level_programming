70f64a9013e5092a7c2249319cdc20d5fe73aa0d
Isaac-7 <isaacokai20@gmail.com>
2022-09-15 19:19:55 -0700

    h

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,12 +11,14 @@ void print_diagonal(int n)
 {
 	int i;
 	int j;
-
+	
+	if ( n > 0)
+	{
 		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < i; j++)
 			{
-				putchar(" ");
+				putchar(' ');
 			}
 
 			putchar('\\');
@@ -27,4 +29,6 @@ void print_diagonal(int n)
 			}
 			putchar('\n');
 		}
+	}
+	putchar('\n');
 }
