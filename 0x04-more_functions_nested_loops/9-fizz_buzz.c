723c339167e4d8a62b75640aec8e2cbe0ebd43f6
Isaac-7 <isaacokai20@gmail.com>
2022-09-15 21:03:23 -0700

    h

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints numbers to 100
+ *
+ * Return: nothing
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 = 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 = 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((i % 3 = 0) && (i % 5 = 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf(i);
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+}
