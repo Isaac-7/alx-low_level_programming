470651e69db0c31ef66bf691ec5d91761c0b1252
Isaac-7 <isaacokai20@gmail.com>
2022-09-19 19:12:43 -0700

    h

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string -> reversee
+ * @s: parameter
+ */
+
+void rev_string(char *s)
+{
+	char tmp;
+	int i;
+	int length1 = 0;
+	int length2 = 0;
+
+	while (s[length1] != '\0')
+	{
+		length1++;
+	}
+
+	length2 = length1 - 1;
+	for (i = 0; i < length1 / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[length2];
+		s[length2] = tmp;
+		length2 -= 1;
+	}
+}
