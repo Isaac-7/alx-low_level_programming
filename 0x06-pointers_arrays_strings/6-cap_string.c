ac4058f18fe8a35314d2baf1b3348b99695f1263
Isaac-7 <isaacokai20@gmail.com>
2022-09-20 15:46:20 -0700

    h

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize char
+ * @str: string to be capitalized
+ * Return: return results
+ */
+
+char *cap_string(char *str)
+{
+	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int len = 13;
+	int a = 0;
+	int i;
+
+	while (str[a])
+	{
+		i = 0;
+		while (i < len)
+		{
+			if ((a == 0 || str[a - 1] == spc[i]) && (str[a] >= 97 && str[a] <= 122))
+			{
+				str[a] = str[a] - 32;
+				i++;
+			}
+			a++;
+		}
+		return (str);
+	}
+}
+
+
+
