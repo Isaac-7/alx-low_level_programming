e8bf69997de91beb3a57e47efa9a709229a135f9
Isaac-7 <isaacokai20@gmail.com>
2022-09-20 14:35:05 -0700

    h

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy -> copy function
+ * @dest: destination
+ * @src: source
+ * @n: limit
+ * Return: string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
