b847bf3cb1866741bf3a149e44eb15ce41115647
Isaac-7 <isaacokai20@gmail.com>
2022-09-19 18:03:22 -0700

    h

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts -> prints
+ * @str: what is printed
+ */
+
+void _puts(char *str);
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
