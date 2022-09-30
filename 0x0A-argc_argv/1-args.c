8a12f4293e5fc9cb4a860aaa284888dfa785482a
Isaac-7 <isaacokai20@gmail.com>
2022-09-30 03:08:29 -0700

    h

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - sth
+ * @argc: h
+ * @argv: sth
+ * Return: o
+ */
+
+int main (int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
