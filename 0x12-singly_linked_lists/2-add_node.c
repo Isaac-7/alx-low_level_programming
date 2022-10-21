8593042abf574453e66d5858337cd3b4d3818cc0
Isaac-7 <isaacokai20@gmail.com>
2022-10-21 06:24:55 -0700

    h

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,13 +16,24 @@ list_t *add_node(list_t **head, const char *str);
 	list_t *new;
 
 	new = malloc(sizeof(list_t));
-	if (new = NULL)
+	if (new == NULL)
 		return (NULL);
 
 	dup = strdup(str);
 	if (dup == NULL)
 	{
 		free(new);
-		return(NULL);
+		return (NULL);
 	}
+
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = dup;
+	new->len = len;
+	new->next = *head;
+
+		*head = new;
+
+	return (new);
 }
