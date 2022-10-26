d95256294cdcd784997c4efdd0f32b780d07569a
Isaac-7 <isaacokai20@gmail.com>
2022-10-25 17:44:57 -0700

    h

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - finds
+ * @head: pointer
+ * @index: index
+ * Return: pinter
+ *
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	size_t n;
+
+	for (n = 0; (n < index) && (head->next); n++)
+		head = head->next;
+
+	if (n < index)
+		return (NULL);
+
+	return (head);
+}
